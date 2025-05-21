1646470c569b7e1b799ea902062f911509000843
Ігнатій bc2024-5 <zignatiusk@gmail.com>
2025-05-21 16:26:06 +0300

    created a js file to store information about the movie

---
+++ src/data/movies.js
@@ -0,0 +1,52 @@
+const movies = [
+  {
+    id: 1,
+    title: "Леон-кілер",
+    description: "Професійний кілер бере під опіку дівчинку.",
+    genre: "Драма, Трилер",
+    poster: "/images/leon.jpg",
+    date: "2025-05-25 20:00"
+  },
+  {
+    id: 2,
+    title: "Хелловін",
+    description: "Маніяк у масці повертається у рідне місто на свято жаху.",
+    genre: "Горор, Трилер",
+    poster: "/images/halloween.jpg",
+    date: "2025-05-26 22:00"
+  },
+  {
+    id: 3,
+    title: "Блейд Ранер",
+    description: "Мисливець на реплікантів розслідує серію втеч штучних людей.",
+    genre: "Наукова фантастика",
+    poster: "/images/bladerunner.jpg",
+    date: "2025-05-27 19:30"
+  },
+  {
+    id: 4,
+    title: "Тихе місце",
+    description: "Родина виживає у світі, де монстри реагують на звук.",
+    genre: "Горор, Фантастика",
+    poster: "/images/quietplace.png",
+    date: "2025-05-28 18:45"
+  },
+  {
+    id: 5,
+    title: "Чужий",
+    description: "Космічний екіпаж стикається з невідомою загрозою.",
+    genre: "Горор, Фантастика",
+    poster: "/images/alien.jpg",
+    date: "2025-05-29 21:15"
+  },
+  {
+    id: 6,
+    title: "Повставший з пекла",
+    description: "Таємнича скринька відкриває двері до світу демонів.",
+    genre: "Горор",
+    poster: "/images/hellraiser.jpg",
+    date: "2025-05-30 23:00"
+  }
+];
+
+export default movies;
