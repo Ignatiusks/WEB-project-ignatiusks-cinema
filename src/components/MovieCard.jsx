85e3a035d5f8ca403d64a535ca721ce5bd42925d
Ігнатій bc2024-5 <zignatiusk@gmail.com>
2025-05-27 02:38:11 +0300

    Added a Book button that redirects to /booking/:id.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
   return (
     <div className="movie-card">
-     <img src={movie.poster} alt={movie.title} />
+      <img src={movie.poster} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>{movie.description}</p>
       <p><strong>Жанр:</strong> {movie.genre}</p>
       <p><strong>Сеанс:</strong> {movie.date}</p>
+      <Link to={`/booking/${movie.id}`}>
+        <button>Забронювати</button>
+      </Link>
     </div>
   );
 };
